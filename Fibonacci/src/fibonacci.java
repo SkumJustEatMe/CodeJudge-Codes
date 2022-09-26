5e9b61205d979b1afde549e9ed8d6c77288d3e7a
Matthias Kobbernagel <Skumfidusendesigns@gmail.com>
2022-09-26 09:02:12 +0200

    Fibonacci tilføjet

---
+++ Fibonacci/src/fibonacci.java
@@ -0,0 +1,23 @@
+public class fibonacci {
+    public static void main(String[] args) {
+        int input, f0, f1, f2, f3, f4, f5, f6;
+        double golden, resultat;
+        golden = 1.618;
+        f0 = 0;
+        f1 = 1;
+        f2 = 1;
+        f3 = 2;
+        f4 = 3;
+        f5 = 5;
+        f6 = 8;
+
+        double test1 = 2;
+        double test2 = 4;
+
+        input = 12;
+        resultat = Math.pow(test1, test2);
+        System.out.print(resultat);
+
+
+    }
+}
