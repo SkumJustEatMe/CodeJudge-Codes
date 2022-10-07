f6445d03604d750d19c896752f3fbdc1a8e5818d
Matthias Kobbernagel <Skumfidusendesigns@gmail.com>
2022-10-07 11:03:09 +0200

    Næsten færdig, Virker nu, men har en fejl

--- Fibonacci/src/fibonacci.java
+++ Fibonacci/src/fibonacci.java
@@ -1,22 +1,22 @@
+import java.util.Scanner;
+
 public class fibonacci {
     public static void main(String[] args) {
-        int input = 12;
-        double golden;
-        golden = 1.618;
-        int count = 0;
-
-        int f1, f2;
-        f1 = 0;
+        Scanner scanner1 = new Scanner(System.in);
+        int input = scanner1.nextInt();
+        int f1, f2, f3;
+        f1 = 1;
         f2 = 1;
 
-        int[] tal = {0,1};
+        int[] tal = new int[input];
+        tal[0] = f1;
+        tal[1] = f2;
 
         for (int i = 0; i < input; i++){
-            int NytTal = tal[f1]+tal[f2];
-            f1++;
-            f2++;
-            System.out.println(NytTal);
-
+            f3 = f1+f2;
+            tal[i+2] = f3;
+            f1 = f2;
+            f2 = f3;
         }
 
 
