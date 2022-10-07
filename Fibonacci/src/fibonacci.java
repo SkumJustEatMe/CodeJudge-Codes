d9bd01b8ceeaa68dd90c9ee93612e5562a5200cc
Matthias Kobbernagel <Skumfidusendesigns@gmail.com>
2022-10-07 11:10:34 +0200

    Færdig. Jeg fjernede array

--- Fibonacci/src/fibonacci.java
+++ Fibonacci/src/fibonacci.java
@@ -5,16 +5,12 @@ public class fibonacci {
         Scanner scanner1 = new Scanner(System.in);
         int input = scanner1.nextInt();
         int f1, f2, f3;
-        f1 = 1;
+        f1 = 0;
         f2 = 1;
 
-        int[] tal = new int[input];
-        tal[0] = f1;
-        tal[1] = f2;
-
         for (int i = 0; i < input; i++){
+            System.out.println(f2);
             f3 = f1+f2;
-            tal[i+2] = f3;
             f1 = f2;
             f2 = f3;
         }
