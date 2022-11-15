d9f692a525a347f6d27490e65cd3fec0f5cd0e3e
Matthias Kobbernagel <Skumfidusendesigns@gmail.com>
2022-11-15 12:00:41 +0100

    Første forsøg på GUI

---
+++ Snake/src/main/java/GUI.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GUI {
+
+    public GUI(){
+        JFrame frame = new JFrame();
+
+
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30,30,30,30));
+        panel.setLayout(new GridLayout(0, 1));
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Test");
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
